b40eade93950bf3060255d6d9b4898d663ad403d
Tomson Kan <tomson.kan@gmail.com>
2019-10-06 12:33:52 -0500

    Final API Fetch project

--- 07week/brew.js
+++ 07week/brew.js
@@ -1,61 +1,39 @@
-// GamepadButton.addEventListener('click', function grabVal() )
+var getSearchText;
 
+function keyup(searchText) {
+  //setting your input text to the global Javascript Variable for every key press
+  console.log(searchText)
+  return getSearchText = searchText;
+
+  //listens for you to press the ENTER key, at which point your web address will change to the one you have input in the search box
+  // if (e.keyCode == 13) {
+  //   window.location = "http://www.myurl.com/search/" + inputTextValue;
+  // }
+}
 
 
 function getBrew() {
-  const proxy = "https://cors-anywhere.herokuapp.com/";
-  const api = `https://sandbox-api.brewerydb.com/v2/beers/?key=5b3a1f0733ab1b492875ba0998e99a2a/?data=30`
-  fetch(proxy + api) 
+  // const proxy = "https://cors-anywhere.herokuapp.com/";
+  // const enterTxt =  
+  const api = `http://api.punkapi.com/v2/beers?beer_name=${getSearchText}`
+  fetch(api) 
       .then(function(response) {
         return response.json()
         console.log(response)
     })
       .then(function(result) {
+        console.log('result', result)
       let output =
-        `<h2>Breweries</h2>`
-        result.data.forEach((function(brew){
+      `<h2> </h2>`
+        result.forEach((function(brew){
           output += `
           <ul>
-          <li>${brew.name} ${brew.id}</li>
-          <li>${brew.nameDisplay}, ${brew.styleId}</li>
-          <li>${brew.labels.icon}</li>
+          <li>${brew.name} id: ${brew.id}</li>
+          <li>${brew.description}, ${brew.ebc}</li>
+          <li>Boil Volume: ${brew.boil_volume.value}</li>
           </ul>
         `
       }))
       document.getElementById("demo").innerHTML = output;
     })
 }
-
-// // This is to add the click feature on my button
-// const button = document.querySelector('button');
-// const subInput = document.querySelector('input');
-// const result = document.querySelector('#result');
-
-// function renderList(json) {
-//   const posts = json.data.children;
-//   return `<ol>
-//     ${posts.data(
-//       post => `<li>${post.data.title} <a href=${post.data.url} target='_blank'>Link</a></li>`
-//     ).join('')}
-//   </ol>`;
-// }
-
-// async function fetchTopFive(sub) {
-//   const URL = `https://sandbox-api.brewerydb.com/v2/beers/?key=5b3a1f0733ab1b492875ba0998e99a2a`;
-//   try {
-//     const fetchResult = fetch(new Request(URL, { method: 'GET', cache: 'reload' }));
-//     const response = await fetchResult;
-//     if (response.ok) {
-//       const jsonData = await response.json();
-//       result.innerHTML = renderList(jsonData);
-//     } else {
-//       result.innerHTML = `Response.status: ${response.status}`;
-//     }
-//   } catch (e) {
-//     result.innerHTML = e;
-//   }
-// }
-
-// button.addEventListener('click', () => {
-//   fetchTopFive(subInput.value);
-// });
